9a0c6f5136dee85b14feab0d2fe36e0ab2967cd5
arwahajahjah <arwahajahja2005@gmail.com>
2024-10-07 21:11:51 +0300

    code format

--- project1.cpp
+++ project1.cpp
@@ -1,62 +1,61 @@
 #include<iostream>
 using namespace std;
 
-const int size = 4;
+const int SIZE = 4;
 
-struct date {
+struct Date {
     int day;
     int month;
     int year;
 };
 
-struct student {
+struct Student {
     int id;
     string name;
     double gpa;
-    date dayofbirth;
-    bool academicprobation;
-    char grade[4];
+    Date dayOfBirth;
+    bool academicProbation;
+    char grades[4];
 };
 
-void read(student students[]) {
-    students[0] = { 998542, "Ahmad", 3.8, {19, 5, 2002}, 0, {'A', 'C', 'F', 'D'} };
-    students[1] = { 986521, "Sara", 3.5, {20, 6, 2002}, 0, {'F', 'B', 'A', 'C'} };
-    students[2] = { 995271, "Rami", 2.8, {18, 1, 2003}, 1, {'C', 'F', 'F', 'A'} };
-    students[3] = { 987741, "Rania", 4.0, {13, 8, 2003}, 0, {'A', 'A', 'A', 'A'} };
+void readStudentsData(Student studentsArray[]) {
+    studentsArray[0] = { 998542, "Ahmad", 3.8, {19, 5, 2002}, 0, {'A', 'C', 'F', 'D'} };
+    studentsArray[1] = { 986521, "Sara", 3.5, {20, 6, 2002}, 0, {'F', 'B', 'A', 'C'} };
+    studentsArray[2] = { 995271, "Rami", 2.8, {18, 1, 2003}, 1, {'C', 'F', 'F', 'A'} };
+    studentsArray[3] = { 987741, "Rania", 4.0, {13, 8, 2003}, 0, {'A', 'A', 'A', 'A'} };
 }
 
-void print(student students[], int size) {
+void printStudentName(Student studentsArray[], int SIZE) {
     cout << "Names of students:" << endl;
-    for (int i = 0; i < size; ++i) {
-        cout << students[i].name << endl;
+    for (int i = 0; i < SIZE; ++i) {
+        cout << studentsArray[i].name << endl;
     }
 }
 
-void countFgrades(student students[]) {
-    int c = 0;
+void countFailedGrades(Student studentsArray[]) {
+    int counter = 0;
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            if (students[i].grade[j] == 'F') {
-                c++;
+            if (studentsArray[i].grades[j] == 'F') {
+                counter++;
             }
         }
-        cout << students[i].name << " " << c << endl;
-        c = 0;
+        cout << studentsArray[i].name << " " << counter << endl;
+        counter = 0;
     }
 }
 
-void checklarger(student students[], int size) {
-    cout << "Students who have day of birth later than 2002:" << endl;
-    for (int i = 0; i < size; ++i) {
-        if (students[i].dayofbirth.year > 2002) {
-            cout << students[i].name << endl;
+void checkAfter2002(Student studentsArray[], int Size) {
+    cout << "Students who have day of birth after 2002:" << endl;
+    for (int i = 0; i < SIZE; ++i) {
+        if (studentsArray[i].dayOfBirth.year > 2002) {
+            cout << studentsArray[i].name << endl;
         }
     }
 }
 
 int main() {
-    const int size = 4;
-    student students[size];
+    Student studentsArray[SIZE];
     int choice;
 
     do {
@@ -64,23 +63,23 @@ int main() {
         cout << "1. Read:\n";
         cout << "2. Print students names:\n";
         cout << "3. Number of F each student has:\n";
-        cout << "4. Name of student who has DOB larger than 2002:\n";
+        cout << "4. Name of student who has day of birth After 2002:\n";
         cout << "5. Exit:\n";
         cout << "Enter option: ";
         cin >> choice;
 
         switch (choice) {
         case 1:
-            read(students);
+            readStudentsData(studentsArray);
             break;
         case 2:
-            print(students, size);
+            printStudentName(studentsArray, SIZE);
             break;
         case 3:
-            countFgrades(students);
+            countFailedGrades(studentsArray);
             break;
         case 4:
-            checklarger(students, size);
+            checkAfter2002(studentsArray, SIZE);
             break;
         case 5:
             cout << "Exiting" << endl;
